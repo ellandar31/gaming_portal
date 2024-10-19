de29ac041150394f10f719865c3070242d3a37b8
ellandar31 <barre.ludovic@gmail.com>
2024-10-19 20:04:34 +0200

    rajout du lanceur de dés

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dice/dice_package.dart';
 import 'package:gaming_portal/firebase/firebase_options.dart';
 import 'package:gaming_portal/src/home.dart';
+import 'package:get/get.dart';
 
 import 'guard/auth_gate.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Get.put(DiceController());//obligatoire pour initialiser le controller
   
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -25,7 +28,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Qwinto',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dice/dice_package.dart';
 import 'package:gaming_portal/src/qwinto/qwinto_page.dart';
 import 'package:gaming_portal/src/qwinto/refresh_button.dart';
 import 'package:gaming_portal/src/qwinto/store/qwinto_state.dart';
@@ -11,15 +12,24 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+     List<Color> diceColors = [
+      Colors.red,
+      Colors.yellow,
+      Colors.purple,
+    ];
     return ChangeNotifierProvider(
         create: (context) => QwintoState(),
         child: Scaffold(
         appBar: AppBar(
           title: const Text('Qwinto App v6'), // Barre en haut
               backgroundColor: Colors.blue, // Couleur de la barre
-          actions: const [
+          actions: [
             //TODO add your actions here 
-            RefreshButton(),
+            const DicesResult(), // Affichage des résultats
+            const SizedBox(width: 20,),
+            ModalDiceLauncher(diceColors: diceColors), // Widget modifié pour GetX        
+            const SizedBox(width: 10,),
+            const RefreshButton(),
             //ProfileButton(),
           ],
           automaticallyImplyLeading: false,
