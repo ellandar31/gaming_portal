99cf17d97564156e5805b3303b6d8a96aa5caff4
ellandar31 <barre.ludovic@gmail.com>
2024-10-19 21:55:40 +0200

    gestion du refresh avec les dés

--- lib/src/qwinto/refresh_button.dart
+++ lib/src/qwinto/refresh_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dice/dice_controller.dart';
 import 'package:gaming_portal/src/qwinto/store/qwinto_state.dart';
+import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 
 class RefreshButton extends StatelessWidget {
@@ -10,11 +12,15 @@ class RefreshButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var qwintoState = Provider.of<QwintoState>(context);
+    List<Color> diceColors = [Colors.red,Colors.yellow,Colors.purple,];
     
-    return IconButton(
-      icon: const Icon(Icons.refresh),
+    final diceController = Get.find<DiceController>();
+    
+    return ElevatedButton(
+      child: const Icon(Icons.refresh),
       onPressed: () {
         qwintoState.reinit();
+        diceController.initDices(diceColors);
       },
     );
   }
