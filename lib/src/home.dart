cea10200b454b855c9c5c608c9eaa503c3befdcc
ellandar31 <barre.ludovic@gmail.com>
2024-10-19 21:56:09 +0200

    traitement du rotate

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -5,37 +5,36 @@ import 'package:gaming_portal/src/qwinto/refresh_button.dart';
 import 'package:gaming_portal/src/qwinto/store/qwinto_state.dart';
 import 'package:provider/provider.dart';
 
-import '../guard/profile_button.dart';
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-     List<Color> diceColors = [
-      Colors.red,
-      Colors.yellow,
-      Colors.purple,
-    ];
+    List<Color> diceColors = [Colors.red,Colors.yellow,Colors.purple,];
+
+    final mediaQuery = MediaQuery.of(context);
+    final isWideScreen = mediaQuery.size.width < mediaQuery.size.height;
+
     return ChangeNotifierProvider(
         create: (context) => QwintoState(),
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Qwinto App v7'), // Barre en haut
-            backgroundColor: Colors.blue, // Couleur de la barre
-            actions: [
-              //TODO add your actions here 
-              const DicesResult(), // Affichage des résultats
-              const SizedBox(width: 20,),
-              ModalDiceLauncher(diceColors: diceColors), // Widget modifié pour GetX        
-              const SizedBox(width: 10,),
-              const RefreshButton(),
-              //ProfileButton(),
-            ],
-            automaticallyImplyLeading: true,
-          ),
-          body: const QwintoPage(),
-      )
+        child: RotatedBox(
+          quarterTurns: isWideScreen ? 3 : 0,
+          child: Scaffold(
+            appBar: AppBar(
+              title: const Text('Qwinto App v7'), // Barre en haut
+              backgroundColor: Colors.blue, // Couleur de la barre
+              actions: [
+                const DicesResult(), // Affichage des résultats des dés
+                const SizedBox(width: 20,),
+                ModalDiceLauncher(diceColors: diceColors),        
+                const SizedBox(width: 10,),
+                const RefreshButton(),
+              ],
+              automaticallyImplyLeading: true,
+            ),
+            body: const QwintoPage(),
+                ),
+        )
     );
   }
 }
